33b69b6f4e2926a91c6a001091fc2dd3d665fce6
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-06-12 11:53:00 +0330

    Improve UI

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -74,19 +74,20 @@ export default function Home() {
             </div>
           </div>
           {/* Blog Section */}
-          <div className='grid grid-cols-6 gap-8 bg-blue-100 md:col-span-9'>
+          <div className='grid grid-cols-6 gap-8 bg-white md:col-span-9'>
             {[
-              'nextjs(1).png',
-              'nextjs(2).png',
-              'nextjs(3).png',
-              'nodejs(1).png',
-              'nodejs(2).png',
+              'nextjs.png',
+              'nuxtjs.png',
               'vuejs.png',
+              'nodejs.png',
+              'reactjs.png',
+              'javascript.png',
+              'tailwind.png',
             ].map((item, index) => {
               return (
                 <div
                   key={index}
-                  className='col-span-6 rounded-3xl bg-red-200 p-2 md:col-span-3 lg:col-span-2'
+                  className='col-span-6 flex flex-col rounded-3xl bg-white p-2 md:col-span-3 lg:col-span-2'
                 >
                   {/* Image: Post Cover */}
                   <div className='aspect-h-9 aspect-w-16 mb-6'>
@@ -96,60 +97,65 @@ export default function Home() {
                       className='h-full w-full rounded-2xl object-cover object-center'
                     />
                   </div>
-                  {/* Title and Description */}
-                  <div className='rounded-2xl bg-gray-50 p-2'>
+                  {/* Blog Content */}
+                  <div className='flex w-full flex-1 flex-col justify-between rounded-2xl bg-gray-100 p-2'>
                     {/* Title */}
                     <h2 className='mb-4 font-bold'>بررسی کامل ریکت</h2>
-                    <div className='mb-2 flex items-center justify-between'>
-                      <div className='flex items-center'>
-                        {/* Image */}
-                        <img
-                          src='/images/nodejs(1).png'
-                          alt=''
-                          className='ml-2 h-6 w-6 rounded-full ring-2 ring-white'
-                        />
-                        {/* Writer's name */}
-                        <span className='text-sm'>امیرحسین</span>
-                      </div>
-                      {/* Category */}
-                      <span className='cursor-pointer rounded-xl bg-blue-100 px-2 py-1 text-xs text-blue-600 transition-all duration-300 hover:bg-blue-600 hover:text-blue-100'>
-                        ری اکت
-                      </span>
-                    </div>
-                    {/* Icons */}
-                    <div className='flex items-center justify-between'>
-                      <div className='flex items-center gap-x-2'>
-                        <button className='flex items-center gap-x-1 rounded-md bg-gray-200 px-1 py-0.5'>
-                          <ChatBubbleBottomCenterTextIcon className='h-5 w-5 stroke-slate-600' />
-                          <span className='text-sm text-slate-600'>4</span>
-                        </button>
-                        <button
-                          className='flex items-center gap-x-1 rounded-md bg-gray-200 px-1 py-0.5'
-                          onClick={() => setIsLiked(!isLiked)}
-                        >
-                          <HeartIcon
-                            className={`h-5 w-5 stroke-red-500 ${
-                              isLiked ? 'fill-red-500' : ''
-                            }`}
+                    {/* Blog Data */}
+                    <div>
+                      <div className='mb-4 flex items-center justify-between'>
+                        <div className='flex items-center'>
+                          {/* Image */}
+                          <img
+                            src='/images/user.png'
+                            alt=''
+                            className='ml-2 h-6 w-6 rounded-full ring-2 ring-white'
                           />
-                          <span className='text-sm text-red-500'>7</span>
-                        </button>
-                        <button
-                          className='flex items-center gap-x-1 rounded-md bg-gray-200 px-1 py-0.5'
-                          onClick={() => setIsBookmarked(!isBookmarked)}
-                        >
-                          <BookmarkIcon
-                            className={`h-5 w-5 stroke-blue-500 ${
-                              isBookmarked ? 'fill-blue-500' : ''
-                            }`}
-                          />
-                        </button>
-                      </div>
-                      <div className='flex items-center'>
-                        <ClockIcon className='h-4 w-4 stroke-slate-500' />
-                        <span className='text-xs text-slate-500'>
-                          زمان مطالعه: 20 دقیقه
+                          {/* Writer's name */}
+                          <span className='text-sm text-slate-600'>
+                            امیرحسین
+                          </span>
+                        </div>
+                        {/* Category */}
+                        <span className='cursor-pointer rounded-xl bg-blue-100 px-2 py-1 text-xs text-blue-600 transition-all duration-300 hover:bg-blue-600 hover:text-blue-100'>
+                          ری اکت
                         </span>
+                      </div>
+                      {/* Icons */}
+                      <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-x-2'>
+                          <button className='flex items-center gap-x-1 rounded-md bg-gray-200 px-1 py-0.5'>
+                            <ChatBubbleBottomCenterTextIcon className='h-4 w-4 stroke-slate-600' />
+                            <span className='text-xs text-slate-600'>4</span>
+                          </button>
+                          <button
+                            className='flex items-center gap-x-1 rounded-md bg-red-200 px-1 py-0.5'
+                            onClick={() => setIsLiked(!isLiked)}
+                          >
+                            <HeartIcon
+                              className={`h-4 w-4 stroke-red-500 ${
+                                isLiked ? 'fill-red-500' : ''
+                              }`}
+                            />
+                            <span className='text-xs text-red-500'>7</span>
+                          </button>
+                          <button
+                            className='flex items-center gap-x-1 rounded-md bg-blue-200 px-1 py-0.5'
+                            onClick={() => setIsBookmarked(!isBookmarked)}
+                          >
+                            <BookmarkIcon
+                              className={`h-4 w-4 stroke-blue-500 ${
+                                isBookmarked ? 'fill-blue-500' : ''
+                              }`}
+                            />
+                          </button>
+                        </div>
+                        <div className='flex items-center'>
+                          <ClockIcon className='h-4 w-4 stroke-slate-500' />
+                          <span className='text-xs text-slate-500'>
+                            زمان مطالعه: 20 دقیقه
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
