0b019c54e2dc21fcc36f0fe72741a9933a4b7db8
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-06-13 12:39:22 +0330

    Add Typography Tailwind Plugin to improve Post Details' Page UI

--- client/src/pages/post/[hashId]/[postSlug].jsx
+++ client/src/pages/post/[hashId]/[postSlug].jsx
@@ -8,68 +8,112 @@ import { toPersianDigits } from '@/utils/toPersianDigits';
 const PostPage = ({ post }) => {
   console.log(post);
   return (
-    <div className='h-screen bg-gray-100'>
-      <header className='mx-auto mb-12 flex max-w-screen-md flex-col gap-y-5 md:flex-row md:items-start md:justify-between'>
-        {/* Author Data */}
-        <div className='flex items-stretch'>
-          <img
-            src='/images/user.png'
-            alt={post.author.name}
-            className='h-14 w-14 rounded-full ring-2 ring-white md:h-20 md:w-20'
-          />
-          <div className='mr-4 flex flex-col justify-between'>
-            <div className='flex'>
-              <span className='text-base font-extrabold'>
-                {post.author.name}
+    <div className='min-h-screen bg-gray-100'>
+      <div className='container mx-auto md:max-w-screen-lg'>
+        <header className='mx-auto mb-12 flex flex-col gap-y-5 md:flex-row md:items-start md:justify-between'>
+          {/* Author Data */}
+          <div className='flex items-stretch'>
+            <img
+              src='/images/user.png'
+              alt={post.author.name}
+              className='h-14 w-14 rounded-full ring-2 ring-white md:h-20 md:w-20'
+            />
+            <div className='mr-4 flex flex-col justify-between'>
+              <div className='flex'>
+                <span className='text-base font-extrabold'>
+                  {post.author.name}
+                </span>
+                <Link href={`/blogs/${post.category.englishTitle}`}>
+                  <a className='mr-2 rounded-full border border-blue-500 bg-white px-3 py-1 text-xs text-blue-500 transition-all duration-300 hover:bg-blue-500 hover:text-white  '>
+                    {post.category.title}
+                  </a>
+                </Link>
+              </div>
+              <span className='hidden text-xs font-normal md:block'>
+                {post.author.biography}
               </span>
-              <Link href={`/blogs/${post.category.englishTitle}`}>
-                <a className='mr-2 rounded-full border border-blue-500 bg-white px-3 py-1 text-xs text-blue-500 transition-all duration-300 hover:bg-blue-500 hover:text-white  '>
-                  {post.category.title}
-                </a>
-              </Link>
-            </div>
-            <span className='hidden text-xs font-normal md:block'>
-              {post.author.biography}
-            </span>
-            <div className='text-myGray-400 text-sm font-normal '>
-              <span>{toLocalDate(post.createdAt)}</span>
-              <span className='mx-1'> &bull;</span>
-              <span>
-                <span> خواندن</span>
-                <span> {toPersianDigits(post.readingTime)} </span>
-                <span>دقیقه </span>
-              </span>
+              <div className='text-myGray-400 text-sm font-normal '>
+                <span>{toLocalDate(post.createdAt)}</span>
+                <span className='mx-1'> &bull;</span>
+                <span>
+                  <span> خواندن</span>
+                  <span> {toPersianDigits(post.readingTime)} </span>
+                  <span>دقیقه </span>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* Interactions Button */}
-        <div className='flex'>
-          <button>
-            <LinkIcon className='h-6 w-6 cursor-pointer text-gray-500 hover:text-black ' />
-          </button>
-          <button className='mr-4 flex items-center rounded-full border border-gray-300 px-3 py-1 text-gray-500 hover:text-gray-600'>
-            <span className='ml-1 text-xs '>
-              {post.isBookmarked ? 'ذخیره شده' : 'ذخیره'}
-            </span>
-            {post.isBookmarked ? (
-              <BookmarkSolidIcon className='h-6 w-6 fill-current' />
-            ) : (
-              <BookmarkIcon className='h-6 w-6 stroke-current' />
-            )}
-          </button>
-        </div>
-
-        {/* <button className='mr-4 flex w-6 items-center rounded-full border border-gray-300 px-3 py-1 text-gray-500 hover:text-gray-600'>
-            <span className='ml-1 text-xs'>
-              {post.isBookmarked ? 'ذخیره شده' : 'ذخیره '}
-            </span>
-            {post.isBookmarked ? (
-              <BookmarkIcon className='h-6 w-6 fill-black stroke-gray-500' />
-            ) : (
-              <BookmarkIcon className='h-6 w-6 stroke-gray-500' />
-            )}
-          </button> */}
-      </header>
+          {/* Interactions Button */}
+          <div className='flex'>
+            <button>
+              <LinkIcon className='h-6 w-6 cursor-pointer text-gray-500 hover:text-black ' />
+            </button>
+            <button className='mr-4 flex items-center rounded-full border border-gray-300 px-3 py-1 text-gray-500 hover:text-gray-600'>
+              <span className='ml-1 text-xs '>
+                {post.isBookmarked ? 'ذخیره شده' : 'ذخیره'}
+              </span>
+              {post.isBookmarked ? (
+                <BookmarkSolidIcon className='h-6 w-6 fill-current' />
+              ) : (
+                <BookmarkIcon className='h-6 w-6 stroke-current' />
+              )}
+            </button>
+          </div>
+        </header>
+        <main
+          className='prose prose-xl prose-slate mx-auto mb-8 max-w-screen-md prose-h1:text-xl prose-h1:font-black prose-h2:text-xl prose-h2:font-extrabold prose-p:text-base prose-p:leading-8 prose-a:text-blue-500 prose-img:rounded-xl md:prose-h1:text-3xl md:prose-h2:text-2xl md:prose-p:text-lg md:prose-p:leading-10
+        '
+        >
+          <h1>{post.title}</h1>
+          <h2>عنوان تستی اول</h2>
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+            متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
+            کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+            راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+            حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود
+            طراحی اساسا مورد استفاده قرار گیرد.
+          </p>
+          <img src={post.coverImage} alt='' />
+          <h2>عنوان تستی دوم</h2>
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+            متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد
+            کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+            راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+            حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود
+            طراحی اساسا مورد استفاده قرار گیرد.
+          </p>
+          <h2>نحوه کانفیگ تیلویند</h2>
+          <p>
+            بدون استفاده از <a href='https://highlightjs.org/'>highlight.js</a>
+            میتوان به سادگی کدها را داخل محتوای بلاگ ها قرار داد!
+          </p>
+          <p>
+            به عنوان مثال، برای کانفیگ تیلویند باید از فایل{' '}
+            <code>tailwind.config.js</code> استفاده کرد:
+          </p>
+          <pre dir='ltr'>{`module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+`}</pre>
+        </main>
+      </div>
     </div>
   );
 };
